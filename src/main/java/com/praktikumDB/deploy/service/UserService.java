f395ba9d217423a8074b561b57fd0df24cd11211
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-04-20 07:25:37 +0700

    add AddUser function to UserService

--- src/main/java/com/praktikumDB/deploy/service/UserService.java
+++ src/main/java/com/praktikumDB/deploy/service/UserService.java
@@ -1,4 +1,21 @@
 package com.praktikumDB.deploy.service;
 
+
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User AddUser(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
 }
